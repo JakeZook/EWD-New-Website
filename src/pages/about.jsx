e9c54cc0d391b11f7c46ce1ed6ddb54787dcd16e
Jake Zook <jakezook01@gmail.com>
2024-06-10 16:31:18 -0600

    Finished content for about page.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
 
 import bg2 from "../assets/ewd_background_2.jpg";
 import bg3 from "../assets/ewd_background_3.jpg";
 
 const About = () => {
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+
+	const navigate = useNavigate();
+
+	const handleLinkClick = (link) => {
+		navigate(`${link}`);
+	};
+
 	return (
 		<div className="bg-black pt-20 sm:pt-20 md:pt-28 lg:pt-32 xl:pt-32">
-			<header className="flex flex-col items-center my-3">
+			<motion.header
+				initial={{ opacity: 0 }}
+				whileInView={{ opacity: 1 }}
+				viewport={{ once: true, amount: 0.25 }}
+				transition={{ duration: 1 }}
+				className="flex flex-col items-center lg:my-3 my-1"
+			>
 				<h1 className="text-primary font-display text-5xl py-3">About Us</h1>
-				<h2 className="text-neutral font-display text-3xl ">
+				<h2 className="text-neutral font-display text-3xl text-center px-3">
 					Welcome to the largest Escape Room in Denver
 				</h2>
-			</header>
-			<main className="my-24">
-				<section className="flex justify-center mx-10 pb-20 border-b-8 border-accent border-double">
+			</motion.header>
+			<main className="mt-24">
+				<motion.section
+					initial={{ opacity: 0 }}
+					whileInView={{ opacity: 1 }}
+					viewport={{ once: true, amount: 0.25 }}
+					transition={{ duration: 1 }}
+					className="flex justify-center mx-10 max-lg:flex-col items-center"
+				>
 					<aside className="flex flex-col items-center">
 						<h2 className="text-5xl text-primary pb-2 mb-5 border-b-8 border-accent border-double">
 							Our Story
@@ -32,21 +57,30 @@ const About = () => {
 							historic ballpark district on 19th and Blake, a block away from
 							Coors Field.
 						</p>
-						<button className="btn btn-primary btn-lg hover:btn-accent mx-3 mt-3">
+						<button
+							onClick={() => handleLinkClick("/contact")}
+							className="btn btn-primary btn-lg hover:btn-accent mx-3 mt-3"
+						>
 							Contact Us
 						</button>
 					</aside>
 					<img
 						src={bg3}
 						alt="A hallway of doors"
-						className="sm:max-w-lg max-lg:max-w-md max-md:max-w-sm max-sm:max-w-56 rounded-lg shadow-2xl mask mask-parallelogram max-lg:mask-circle max-md:mt-3"
+						className="sm:max-w-lg max-lg:hidden max-md:max-w-sm max-sm:max-w-56 rounded-lg shadow-2xl mask mask-parallelogram max-lg:mask-circle max-md:mt-3"
 					/>
-				</section>
-				<section className="flex justify-center mx-10 mt-20">
+				</motion.section>
+				<motion.section
+					initial={{ opacity: 0 }}
+					whileInView={{ opacity: 1 }}
+					viewport={{ once: true, amount: 0.25 }}
+					transition={{ duration: 1 }}
+					className="flex justify-center mx-10 mt-20 max-lg:flex-col items-center"
+				>
 					<img
 						src={bg2}
 						alt="A hallway of doors"
-						className="sm:max-w-lg max-lg:max-w-md max-md:max-w-sm max-sm:max-w-56 rounded-lg shadow-2xl mask mask-parallelogram-2 max-lg:mask-circle max-md:mt-3 border-8 border-accent"
+						className="sm:max-w-lg max-lg:max-w-md max-md:max-w-sm max-sm:max-w-56 rounded-lg shadow-2xl mask mask-parallelogram-2 max-lg:mask-circle max-md:mt-3 max-lg:mb-3"
 					/>
 					<aside className="flex flex-col items-center">
 						<h2 className="text-5xl text-primary pb-2 mb-5 border-b-8 border-accent border-double">
@@ -66,11 +100,50 @@ const About = () => {
 							excitement. When visiting us, guests can choose from the following
 							options:
 						</p>
-						<button className="btn btn-primary btn-lg hover:btn-accent mx-3 mt-3">
+						<button
+							onClick={() => handleLinkClick("/all-rooms")}
+							className="btn btn-primary btn-lg hover:btn-accent mx-3 mt-3"
+						>
 							All Rooms
 						</button>
 					</aside>
-				</section>
+				</motion.section>
+				<motion.div
+					initial={{ opacity: 0 }}
+					whileInView={{ opacity: 1 }}
+					viewport={{ once: true, amount: 0.25 }}
+					transition={{ duration: 1 }}
+					className="flex justify-center my-10"
+				>
+					<div className="flex flex-col items-center w-1/3">
+						<h5 className="text-primary text-5xl pb-3 font-display">Socials</h5>
+						<div className="flex max-sm:items-center">
+							<SocialIcon
+								className="m-3 hover:opacity-80"
+								network="facebook"
+								url="www.facebook.com"
+								href="https://www.facebook.com/escapeworksdenver/"
+								target="_blank"
+							/>
+							<SocialIcon
+								className="m-3 hover:opacity-80"
+								network="tiktok"
+								url="www.tiktok.com"
+								bgColor="white"
+								fgColor="black"
+								href="https://www.tiktok.com/@escapeworksdenver"
+								target="_blank"
+							/>
+							<SocialIcon
+								className="m-3 hover:opacity-80"
+								network="instagram"
+								url="www.instagram.com"
+								href="https://www.instagram.com/escapeworks_denver/"
+								target="_blank"
+							/>
+						</div>
+					</div>
+				</motion.div>
 			</main>
 		</div>
 	);
